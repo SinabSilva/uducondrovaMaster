198a94c19b71bcb9eb5c7c45df4f64d5a31ca4c2
sinabSilva <sinabns@hotmail.com>
2021-08-14 11:45:35 -0300

    sinab

---
+++ Sprint Tech/teste30/src/test/java/teste30/Teste01.java
@@ -0,0 +1,24 @@
+package teste30;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Teste01 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Sprint Tech/teste40/src/test/java/teste40/testando03.java
@@ -0,0 +1,24 @@
+package teste40;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testando03 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
